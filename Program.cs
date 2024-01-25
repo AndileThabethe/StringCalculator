a286f32d8bb5b169174423f16ef10da27e8079ce
AndileThabethe <151631281+AndileThabethe@users.noreply.github.com>
2024-01-25 14:04:40 +0200

    File hierarchy adjustment.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 public class StringCalculator
 {
     public int Add(string input)
-    {  
+    {
         MatchCollection numericSubstrings = Regex.Matches(input, @"-?\d+");
 
         List<int> parsedNumericSubstrings = numericSubstrings.Cast<Match>()
@@ -18,9 +19,16 @@ public class StringCalculator
             throw new Exception("Negatives not allowed: " + string.Join(" ", negativeNumbers));
         }
 
-        // Calculate the sum of the non-negative numbers, ignoring numbers greater than 1000
         int sum = parsedNumericSubstrings.Where(n => n >= 0 && n <= 1000).Sum();
 
         return sum;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
     }
 }
